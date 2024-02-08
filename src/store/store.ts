2c612b599b586ebfbaf38b309a0f781684ec43dc
Rhae L <85597883+Rha3L@users.noreply.github.com>
2024-02-08 12:52:38 +1100

    Update store.ts

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,19 +1,17 @@
-import { combineReducers,configureStore } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../features/user/userSlice";
 import cartReducer from "../features/cart/cartSlice";
 
-const rootReducer = combineReducers({
+const store = configureStore({
+  reducer: {
     user: userReducer,
     cart: cartReducer,
-})
+  },
+});
 
-export function setupStore(preloadedState?: Partial<RootState>) {
-  return configureStore({
-    reducer: rootReducer,
-    preloadedState
-  })
-}
-export type RootState = ReturnType<typeof rootReducer>;
-export type AppStore = ReturnType<typeof setupStore>;
-export type AppDispatch = AppStore['dispatch'];
+export type RootState = ReturnType<typeof store.getState>;
+
+export type AppDispatch = typeof store.dispatch;
+
+export default store;
 
